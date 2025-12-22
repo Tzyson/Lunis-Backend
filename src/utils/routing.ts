36c56f18c863a55f9a4120b0a1a34bab269fa041
Tzyson <146991344+Tzyson@users.noreply.github.com>
2025-12-22 10:35:17 +0100

    oops

--- src/utils/routing.ts
+++ src/utils/routing.ts
@@ -1,5 +1,17 @@
 import region from "../routes/region/region";
+import waitingroom from "../routes/waitingroom/waitingroom";
+import lightswitch from "../routes/lightswitch/lightswitch";
+import socialbans from "../routes/socialbans/socialbans";
+import version from "../routes/version/version";
+import timeline from "../routes/timeline/timeline";
+import datarouter from "../routes/datarouter/datarouter";
 
 export default function () {
     region();
+    waitingroom();
+    lightswitch();
+    socialbans();
+    version();
+    timeline();
+    datarouter();
 }
